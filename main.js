63eee323e3765aba8f7cf7738e822fd0e9272981
Emily Deans <emilyldeans@gmail.com>
2017-07-17 14:35:58 -0400

    Added HTML and template literal to JS

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 let container = document.querySelector('.contentHere');
 
 let req = new XMLHttpRequest();
-req.open("GET", "https://api.github.com/");
+req.open("GET", "https://api.github.com/users/deansy11");
+req.addEventListener("load", reqListener);
 req.send();
+
+function reqListener () {
+  let data = JSON.parse(this.responseText);
+  let accountInfo = `My name is ${data.name} and I am from ${data.location}`
+  console.log(data);
+}
+
+let template = `
+
+`
+
+
+
+
+
+// a7890b2f1b5090ada952539d535e04a5c68f5468
