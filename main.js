dd6b1f1df0133fe5fde67e33c6721d758a54ee69
Emily Deans <emilyldeans@gmail.com>
2017-07-17 14:57:54 -0400

    Added HTML with template literal

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let container = document.querySelector('.contentHere');
+let container = document.querySelector('.content');
 
 let req = new XMLHttpRequest();
 req.open("GET", "https://api.github.com/users/deansy11");
@@ -7,16 +7,24 @@ req.send();
 
 function reqListener () {
   let data = JSON.parse(this.responseText);
-  let accountInfo = `My name is ${data.name} and I am from ${data.location}`
-  console.log(data);
-}
 
-let template = `
-
-`
-
-
-
-
-
-// a7890b2f1b5090ada952539d535e04a5c68f5468
+  let template = `
+    <h1>${data.name}</h1>
+    <div class="basics">
+        <ul>
+          <li>Name: ${data.name}</li>
+          <li>GitHub URL: ${data.html_url}</li>
+          <li>Email: emilyldeans@gmail.com</li>
+          <li>Company: ${data.company}</li>
+          <li>Website: ${data.blog}</li>
+        </ul>
+    </div>
+    <div class="story">
+        <p>${data.bio}</p>
+    </div>
+    <div class="avatar">
+      <img src="${data.avatar_url}" alt="profile pic">
+    </div>
+  `
+  container.innerHTML = template;
+};
