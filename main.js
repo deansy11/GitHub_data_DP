93315edb6285a9b61b5b264bb4d651ff8b7fe332
Emily Deans <emilyldeans@gmail.com>
2017-07-17 15:16:40 -0400

    Added flexbox and formatted image

--- main.js
+++ main.js
@@ -10,21 +10,25 @@ function reqListener () {
 
   let template = `
     <h1>${data.name}</h1>
-    <div class="basics">
-        <ul>
-          <li>Name: ${data.name}</li>
-          <li>GitHub URL: ${data.html_url}</li>
-          <li>Email: emilyldeans@gmail.com</li>
-          <li>Company: ${data.company}</li>
-          <li>Website: ${data.blog}</li>
-        </ul>
-    </div>
-    <div class="story">
-        <p>${data.bio}</p>
-    </div>
-    <div class="avatar">
-      <img src="${data.avatar_url}" alt="profile pic">
-    </div>
+    <div class="totalContent">
+        <div class="basics">
+          <h3>The Basics</h3>
+            <ul>
+              <li>Name: ${data.name}</li>
+              <li>GitHub URL: ${data.html_url}</li>
+              <li>Email: emilyldeans@gmail.com</li>
+              <li>Company: ${data.company}</li>
+              <li>Website: ${data.blog}</li>
+            </ul>
+        </div>
+        <div class="story">
+          <h3>The Story</h3>
+            <p>${data.bio}</p>
+        </div>
+        <div class="avatar">
+          <img src="${data.avatar_url}" alt="profile pic">
+        </div>
+      </div>
   `
   container.innerHTML = template;
 };
