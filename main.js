f131f5efd444c0da3f84893ba1f2c99578d65b26
Emily Deans <emilyldeans@gmail.com>
2017-07-19 16:10:57 -0400

    Added styling elements on form inputs

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
 let container = document.querySelector('.content');
 
-let req = new XMLHttpRequest();
-req.open("GET", "https://api.github.com/users/deansy11");
-req.addEventListener("load", reqListener);
-req.send();
-
-function reqListener () {
-  let data = JSON.parse(this.responseText);
-
-  let template = `
-    <h1>${data.name}</h1>
-    <div class="totalContent">
+fetch("https://api.github.com/users/deansy11")
+  .then(
+    function(response) {
+      if(response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        console.log(data);
+        let template = `
+        <h1>${data.name}</h1>
+        <div class="totalContent">
         <div class="basics">
-          <h3>The Basics</h3>
-            <ul>
-              <li><span>Name: </span>${data.name}</li>
-              <li><span>GitHub URL: </span>${data.html_url}</li>
-              <li><span>Company: </span>The Iron Yard</li>
-              <li><span>Website: </span>${data.blog}</li>
-            </ul>
+        <h3>The Basics</h3>
+        <ul>
+        <li><span>Name: </span>${data.name}</li>
+        <li><span>GitHub URL: </span>${data.html_url}</li>
+        <li><span>Company: </span>The Iron Yard</li>
+        <li><span>Website: </span>${data.blog}</li>
+        </ul>
         </div>
         <div class="story">
-          <h3>The Story</h3>
-            <p>${data.bio}</p>
+        <h3>The Story</h3>
+        <p>${data.bio}</p>
         </div>
         <div class="avatar">
-          <img src="${data.avatar_url}" alt="profile pic">
+        <img src="${data.avatar_url}" alt="profile pic">
         </div>
-      </div>
-  `
-  container.innerHTML = template;
-};
+        </div>
+        `
+        container.innerHTML = template;
+      });
+    }
+  )
+
+// let req = new XMLHttpRequest();
+// req.open("GET", "https://api.github.com/users/deansy11");
+// req.addEventListener("load", reqListener);
+// req.send();
+
+// function reqListener () {
+//   let data = JSON.parse(this.responseText);
